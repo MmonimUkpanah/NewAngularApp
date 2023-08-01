7d5160c10eabeedde43a287be271dea90bd8740f
Mmonim Ukpanah <mukpanah22@gmail.com>
2023-08-01 03:03:42 +0100

    made changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path: '',pathMatch: 'full', redirectTo: '/login/loginAuthentication'},
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
-  
+  { path: 'content', loadChildren: () => import('./content/content.module').then(m => m.ContentModule) }
 ];
 
 @NgModule({
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login.component';
 
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from './login.component';
 
 
 @NgModule({
@@ -9,7 +10,8 @@ import { LoginComponent } from './login.component';
     LoginComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    LoginRoutingModule
   ]
 })
 export class LoginModule { }
